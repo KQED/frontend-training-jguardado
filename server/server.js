9eb24e0fe4a9ac2b26697371b9a4700adf0519e9
Jmg047 <jmg52311@gmail.com>
2023-03-09 11:20:37 -0800

    feat(KSM-4227,  Barebones Node/Express App): added 'hello world' from express/node backend for the frontend training tickets.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get("/api", (req, res) => {
-    res.json({ "users": ["User1", "User2", "User3"]})
+app.get("/*", (req, res) => {
+    res.send("Hello world")
 })
 
 app.listen(5001, () => { console.log("Server started on port 5001")})
